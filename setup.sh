f13acb6f715cd9d1f1844d25c3757f72f5892dd2
callum <callum@users.noreply.github.com>
2020-10-26 11:43:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,27 +4,11 @@ mkdir ~/Projects
 brew install git
 brew install heroku/brew/heroku
 brew install hub
-brew install node
+brew install nvm
 
 # apps
 brew cask install 1password
-brew cask install abstract
 brew cask install appcleaner
-brew cask install atom
-brew cask install docker
-brew cask install google-chrome
-brew cask install insomnia
-brew cask install sketch
-brew cask install slack
+brew cask install microsoft-teams
 brew cask install telegram
-
-# atom packages
-apm install flow-ide
-apm install language-babel
-apm install language-elixir
-apm install linter-eslint
-apm install multi-cursor
-apm install pigments
-apm install prettier-atom
-apm install sort-lines
-apm install tomorrow-night-eighties-syntax
+brew cask install visual-studio-code
