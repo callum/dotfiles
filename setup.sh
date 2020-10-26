30a1bf1b55231a9020e6240804a8e88c62955b1a
callum <callum@users.noreply.github.com>
2020-10-26 13:14:23 +0000

    Add Oracle JDK

--- setup.sh
+++ setup.sh
@@ -11,5 +11,6 @@ brew cask install 1password
 brew cask install appcleaner
 brew cask install google-cloud-sdk
 brew cask install microsoft-teams
+brew cask install oracle-jdk
 brew cask install telegram
 brew cask install visual-studio-code
