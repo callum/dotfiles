25c33c5e463889f888af3bd3ac84dce4ffef7095
callum <callum@users.noreply.github.com>
2020-10-26 12:34:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 mkdir ~/Projects
 
 # command-line tools
+brew install firebase-cli
 brew install git
-brew install heroku/brew/heroku
 brew install hub
 brew install nvm
 
 # apps
 brew cask install 1password
 brew cask install appcleaner
+brew cask install google-cloud-sdk
 brew cask install microsoft-teams
 brew cask install telegram
 brew cask install visual-studio-code
